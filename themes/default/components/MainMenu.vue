c0b5c4878e5c9f31e7e733ec25bfc748c7289cdb
David Meir-Levy <davidmeirlevy@gmail.com>
2019-08-27 17:23:50 +0300

    scrollable main menu

--- themes/default/components/MainMenu.vue
+++ themes/default/components/MainMenu.vue
@@ -1,7 +1,9 @@
 <template>
-	<div class="menu">
-		<nuxt-link v-for="(item, index) of menu" :to="item.route" :key="index">{{item.name}}
-		</nuxt-link>
+	<div class="menu-wrapper">
+		<div class="menu">
+			<nuxt-link v-for="(item, index) of menu" :to="item.route" :key="index">{{item.name}}
+			</nuxt-link>
+		</div>
 	</div>
 </template>
 <script>
@@ -17,13 +19,20 @@
 	}
 </script>
 <style scoped>
+	.menu-wrapper {
+		display: block;
+		overflow: auto;
+
+	}
 	.menu {
+		flex-wrap: nowrap;
 		display: flex;
 	}
 
 	a {
 		padding: 5px 10px;
 		height: 35px;
+		white-space: nowrap;
 	}
 
 	a.nuxt-link-active, a:hover {

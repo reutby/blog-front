d39967bd683d9ca90990f362ffb8e468ba95cd99
David Meir-Levy <davidmeirlevy@gmail.com>
2019-09-26 22:44:13 +0300

    proxy to assets service

--- server/api-proxy.js
+++ server/api-proxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware')
-const { authService, contentService, adminPanel } = require('../config')
+const { authService, contentService, adminPanel, assetsService } = require('../config')
 
 /**
  * This function is a temporary thingy because I'm too lazy right now
@@ -8,33 +8,33 @@ const { authService, contentService, adminPanel } = require('../config')
  * @param app: Express.Application
  */
 module.exports = function apiProxy (app) {
-	const authProxy = proxy({
-		target: `${authService.protocol}://${authService.url}:${authService.port}`,
-		changeOrigin: true
-	})
-	const contentProxy = proxy({
-		target: `${contentService.protocol}://${contentService.url}:${contentService.port}`,
-		changeOrigin: true
-	})
+  const authProxy = proxy({
+    target: `${authService.protocol}://${authService.url}:${authService.port}`,
+    changeOrigin: true
+  })
+  const contentProxy = proxy({
+    target: `${contentService.protocol}://${contentService.url}:${contentService.port}`,
+    changeOrigin: true
+  })
 
-	const adminProxy = proxy({
-		target: `${adminPanel.protocol}://${adminPanel.url}:${adminPanel.port}`,
-		changeOrigin: true
-	})
+  const adminProxy = proxy({
+    target: `${adminPanel.protocol}://${adminPanel.url}:${adminPanel.port}`,
+    changeOrigin: true
+  })
 
+  const assetsProxy = proxy({
+    target: `${assetsService.protocol}://${assetsService.url}:${assetsService.port}`,
+    changeOrigin: true
+  })
 
-	const assetsProxy = proxy({
-		target: `${adminPanel.protocol}://${adminPanel.url}:${adminPanel.port}`,
-		changeOrigin: true
-	})
-
-	app.use('/api/signin', authProxy)
-	app.use('/api/signup', authProxy)
-	app.use('/api/token', authProxy)
-	app.use('/api/me', authProxy)
-	app.use('/api/users', authProxy)
-	app.use('/api/categories', contentProxy)
-	app.use('/api/posts', contentProxy)
-	app.use('/api/menus', contentProxy)
-	app.use('/gp-admin', adminProxy)
+  app.use('/api/signin', authProxy)
+  app.use('/api/signup', authProxy)
+  app.use('/api/token', authProxy)
+  app.use('/api/me', authProxy)
+  app.use('/api/users', authProxy)
+  app.use('/api/categories', contentProxy)
+  app.use('/api/posts', contentProxy)
+  app.use('/api/menus', contentProxy)
+  app.use('/api/assets', assetsProxy)
+  app.use('/gp-admin', adminProxy)
 }

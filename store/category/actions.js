bc3e999538c02404d317c507b21c33d3fa2305b7
David Meir-Levy <davidmeirlevy@gmail.com>
2020-03-09 14:59:14 +0200

    fix refresh frontend between categories

--- store/category/actions.js
+++ store/category/actions.js
@@ -1,18 +1,22 @@
-import {MUTATIONS, ACTIONS} from './consts';
+import { MUTATIONS, ACTIONS, DATA } from './consts'
 
 export default {
-  [ACTIONS.INIT]({dispatch}, category) {
+  [ACTIONS.INIT] ({ dispatch, commit, state }, category) {
+    if (state[DATA.METADATA].path !== category) {
+      commit(MUTATIONS.SET_METADATA, {})
+      commit(MUTATIONS.SET_POSTS, [])
+    }
     return Promise.all([
       dispatch(ACTIONS.LOAD_POSTS, category),
       dispatch(ACTIONS.LOAD_CATEGORY, category)
-    ]);
+    ])
   },
-  [ACTIONS.LOAD_POSTS]({commit}, category) {
+  [ACTIONS.LOAD_POSTS] ({ commit }, category) {
     return this.$axios.$get(`api/categories/${category}/posts?isPublic=true`)
-      .then(list => commit(MUTATIONS.SET_POSTS, list));
+      .then(list => commit(MUTATIONS.SET_POSTS, list))
   },
-  [ACTIONS.LOAD_CATEGORY]({commit}, category) {
+  [ACTIONS.LOAD_CATEGORY] ({ commit }, category) {
     return this.$axios.$get(`api/categories/${category}`)
-      .then(metadata => commit(MUTATIONS.SET_METADATA, metadata));
+      .then(metadata => commit(MUTATIONS.SET_METADATA, metadata))
   }
-};
+}
